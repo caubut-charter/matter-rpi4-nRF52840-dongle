921cc183edcd49cd149b1fcd2ed483826c2ab533
Christopher Aubut <christopher.aubut@charter.com>
2021-10-12 23:32:10 -0600

    docs: save point

--- etc/docker/avahi/avahi-utils/entrypoint.sh
+++ etc/docker/avahi/avahi-utils/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 service dbus start 1> /dev/null
+
 rm /run/avahi-daemon/pid
 service avahi-daemon start 1> /dev/null
-
 service avahi-daemon status
+
 sleep 2
 
 exec "$@"
--- etc/docker/connectedhomeip/environment/entrypoint.sh
+++ etc/docker/connectedhomeip/environment/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 service dbus start 1> /dev/null
+
+service avahi-daemon start 1> /dev/null
+
 bluetoothd &
 
 exec "$@"
+
+# TODO net <= network; can use host?
+# TODO matter-bridge ip table
+# TODO device doesn't need double?
